83113e65eb691169d1b725693902ae2f4f25086c
flo <flo.langeder@gmail.com>
2021-01-08 16:09:21 +0100

    moved token and channel whitelist to config file

--- bot.js
+++ bot.js
@@ -1,57 +1,83 @@
 const fetch = require('node-fetch');
-const { Client, MessageAttachment } = require('discord.js');
+const fs = require('fs');
+const Discord = require('discord.js');
 
-// DISCORD
-var client = new Client();
-const token = 'Nzk3MDkyMjg4NjAxMjYwMDQ0.X_hcCg.tOBo_tug88CGrOnEG3N-LAA-Qd8';
+let config = {};
+try {
+  const configRaw = fs.readFileSync('config.json');
+  config = JSON.parse(configRaw);
+} catch (e) {
+  console.log(`Failed to parse config: ${e}`);
+  process.exit(1);
+}
+
+const discordClient = new Discord.Client();
+const discordToken = config.discordToken;
 
 const redditUrl = 'https://www.reddit.com/r/';
+const redditMediaDomainWhitelist = ['https://redgifs.com', 'https://gfycat.com', 'https://v.redd.it'];
+const redditMediaExtensionWhitelist = ['.jpg', '.jpeg', '.gifv', '.gif'];
+discordClient.login(discordToken);
 
-client.login(token);
-
-client.on("ready", () => {
+discordClient.on("ready", () => {
   console.log("Ready!");
 });
 
+const filterRedditResponse = (({ json }) => {
+  return json.filter(post => {
+    const url = post.data.url;
+
+    let found = false;
+    redditMediaDomainWhitelist.forEach(domain => {
+      if (!found && url.includes(domain)) {
+        found = true;
+      }
+    });
+    redditMediaExtensionWhitelist.forEach(extension => {
+      if (!found && url.endsWith(extension)) {
+        found = true;
+      }
+    });
+
+    return found;
+  });
+});
+
 const redditRequest = async ({ subreddit, msg }) => {
+  if (!config.discordChannelWhitelist.includes(msg.channel.id)) return Promise.resolve();
+
   const result = await fetch(redditUrl + subreddit + '.json');
   if (result.status !== 200) {
     if (result.status === 404) {
       msg.channel.send("Subreddit not found");
-      return;
+    } else {
+      msg.channel.send(`Request to reddit failed: \n\`\`\`Markdown\nURL: ${result.url}\nStatus: ${result.status}, ${result.statusText}\`\`\``);
     }
-    msg.channel.send(`Request to reddit failed: \n\`\`\`Markdown\nURL: ${result.url}\nStatus: ${result.status}, ${result.statusText}\`\`\``);
+    return Promise.resolve();
   }
+
   const body = await result.json();
   let json = body.data.children;
   if (json.length == 0) {
     msg.channel.send('Subreddit is empty');
-  } else {
-    //get content
-    let contentList = [];
-    for (let i = 0; i < json.length; i++) {
-      console.log(json[i].data.url);
-      if (json[i].data.url.endsWith('.jpg') || json[i].data.url.endsWith('.jpeg') || json[i].data.url.endsWith('.gifv') || json[i].data.url.endsWith('.gif')
-        || json[i].data.url.startsWith('https://redgifs.com')) {
-        contentList.push(json[i].data.url);
-      }
-    }
-
-    if (contentList.length == 0) {
-      msg.channel.send('Nothing found');
-    }
-    else {
-      //decide random
-      const file = contentList[Math.floor(Math.random() * contentList.length)];
-      const attachment = new MessageAttachment(file);
-      msg.channel.send(attachment);
-    }
+    return Promise.resolve();
   }
+
+  const contentList = filterRedditResponse({ json });
+
+  if (contentList.length == 0) {
+    msg.channel.send('Nothing found');
+    return Promise.resolve();
+  }
+
+  const post = contentList[Math.floor(Math.random() * contentList.length)];
+  return msg.channel.send(post.data.url);
 }
 
-
-client.on('message', async msg => {
+discordClient.on('message', async msg => {
+  // filter messages posted from bot
   if (msg.author.bot) return;
+
   if (!msg.content.startsWith('r!')) return;
 
   const subreddit = msg.content.split('r!')[1];
